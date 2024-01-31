f2b71361d7e6e0279d326ca32d0cb37f6f033211
lagrangeluo <654186749@qq.com>
2024-01-31 17:47:57 +0800

    v1.1

---
+++ rm_75_robot/rm_75_driver/cmake-build-debug/CMakeFiles/rm_driver.dir/DependInfo.cmake
@@ -0,0 +1,51 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/ubuntu/ws_rm_75_robot/src/rm_75_robot/rm_driver/src/cJSON.c" "/home/ubuntu/ws_rm_75_robot/src/rm_75_robot/rm_driver/cmake-build-debug/CMakeFiles/rm_driver.dir/src/cJSON.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "ROSCONSOLE_BACKEND_LOG4CXX"
+  "ROS_BUILD_SHARED_LIBS=1"
+  "ROS_PACKAGE_NAME=\"rm_driver\""
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/home/ubuntu/ws_rm_75_robot/devel/.private/rm_msgs/include"
+  "/home/ubuntu/ws_rm_75_robot/src/rm_75_robot/rm_msgs/include"
+  "/opt/ros/melodic/include"
+  "/opt/ros/melodic/share/xmlrpcpp/cmake/../../../include/xmlrpcpp"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ubuntu/ws_rm_75_robot/src/rm_75_robot/rm_driver/src/rm_driver.cpp" "/home/ubuntu/ws_rm_75_robot/src/rm_75_robot/rm_driver/cmake-build-debug/CMakeFiles/rm_driver.dir/src/rm_driver.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "ROSCONSOLE_BACKEND_LOG4CXX"
+  "ROS_BUILD_SHARED_LIBS=1"
+  "ROS_PACKAGE_NAME=\"rm_driver\""
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/ubuntu/ws_rm_75_robot/devel/.private/rm_msgs/include"
+  "/home/ubuntu/ws_rm_75_robot/src/rm_75_robot/rm_msgs/include"
+  "/opt/ros/melodic/include"
+  "/opt/ros/melodic/share/xmlrpcpp/cmake/../../../include/xmlrpcpp"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ rm_75_robot/rm_75_driver/cmake-build-debug/gtest/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /usr/src/googletest
+# Build directory: /home/ubuntu/ws_rm_75_robot/src/rm_75_robot/rm_driver/cmake-build-debug/gtest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
